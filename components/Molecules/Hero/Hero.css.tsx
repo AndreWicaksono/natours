6f29899664e5393c698828c8cc23c0c2a7626f39
Andre Wicaksono <andrewicaksana@gmail.com>
2022-04-09 16:46:37 +0700

    Added <Hero />

---
+++ components/Molecules/Hero/Hero.css.tsx
@@ -0,0 +1,99 @@
+import styled from "@emotion/styled";
+
+export const Button = styled.button`
+  background-color: #fff;
+  color: #777;
+
+  animation: moveInBottom 0.5s ease-out 0.75s;
+  animation-fill-mode: none;
+  -webkit-animation-fill-mode: backwards;
+  animation-fill-mode: backwards;
+  text-transform: uppercase;
+  text-decoration: none;
+  padding: 1rem 2.5rem;
+  display: inline-block;
+  border-radius: 10rem;
+  -webkit-transition: all 0.2s;
+  transition: all 0.2s;
+  position: relative;
+  font-size: 1rem;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-3px);
+    -webkit-box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.2);
+    box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const HeroBase = styled.header`
+  position: relative;
+
+  height: 40vh;
+
+  @media only screen and (min-width: 1024px) {
+    height: 50vh;
+  }
+
+  @media only screen and (min-width: 1280px) {
+    height: 65vh;
+  }
+
+  .heading-primary {
+    color: #fff;
+    text-transform: uppercase;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    text-align: center;
+
+    @media only screen and (min-width: 1024px) {
+      text-align: unset;
+    }
+
+    &--main {
+      display: block;
+      font-weight: 400;
+      letter-spacing: 1rem;
+      -webkit-animation-name: moveInLeft;
+      animation-name: moveInLeft;
+      -webkit-animation-duration: 1s;
+      animation-duration: 1s;
+      -webkit-animation-timing-function: ease-out;
+      animation-timing-function: ease-out;
+
+      @media only screen and (min-width: 768px) {
+        letter-spacing: 2rem;
+      }
+    }
+
+    &--sub {
+      display: block;
+      font-weight: 700;
+      letter-spacing: 0.5rem;
+      -webkit-animation: moveInRight 1s ease-out;
+
+      @media only screen and (min-width: 768px) {
+        letter-spacing: 1rem;
+      }
+    }
+  }
+
+  &:before {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+
+    background: linear-gradient(
+      to right bottom,
+      rgba(126, 213, 111, 0.8),
+      rgba(40, 180, 133, 0.8)
+    );
+    content: "";
+    z-index: 1;
+  }
+`;
+
+export default HeroBase;
---
+++ components/Molecules/Hero/Hero.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+
+import HeroBase, { Button } from "./Hero.css";
+
+const Hero: React.FC<{ srcImage: string }> = ({ srcImage }) => {
+  return (
+    <HeroBase>
+      <Image
+        alt="Natours Hero's Image"
+        layout="fill"
+        objectFit="cover"
+        src={srcImage}
+      />
+
+      <div className="absolute inset-0 z-10">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="heading-primary mb-10">
+            <span className="heading-primary--main text-3xl lg:text-6xl pb-6">
+              Outdoors
+            </span>
+            <span className="heading-primary--sub text-sm lg:text-xl">
+              is where life happens
+            </span>
+          </h1>
+
+          <Button>Discover our tours</Button>
+        </div>
+      </div>
+    </HeroBase>
+  );
+};
+
+export default Hero;
